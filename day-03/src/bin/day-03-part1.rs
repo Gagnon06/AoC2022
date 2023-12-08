791da16b70a2a14b1fddf0cfc14575068c4a8610
Leonard Gagnon <leonard.gagnon@gmail.com>
2023-12-08 16:10:58 -0500

    Completed day 03

---
+++ day-03/src/bin/day-03-part1.rs
@@ -0,0 +1,68 @@
+#![feature(test)]
+
+extern crate test;
+
+fn item_to_priority(item: char) -> u32 {
+    if item as u32 >= 'a' as u32 {
+        return (item as u32) - ('a' as u32) + 1;
+    }
+
+    (item as u32) - ('A' as u32) + 27
+}
+
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    let priorities: Vec<u32> = input.lines()
+        .filter_map(|line| {
+            let compartments = line.split_at(line.len()/2);
+            let matching: Vec<char> = compartments.0.chars()
+                .filter_map(|a| {
+                    compartments.1.find(a).is_some().then_some(a)
+                })
+                .collect();
+            (matching.len() >= 1).then_some(item_to_priority(matching[0]))
+        })
+        .collect();
+
+    priorities.iter().sum::<u32>().to_string()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_example1() {
+        let result = part1(
+            "\
+            vJrwpWtwJgWrhcsFMMfFFhFp\n\
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+            PmmdzqPrVvPwwTWBwg\n\
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+            ttgJtRGJQctTZtZT\n\
+            CrZsJsPPZsGzwwsLwLmpwMDw");
+        assert_eq!(result, "157");
+    }
+
+    #[test]
+    fn test_part1_real_data() {
+        let input = include_str!("../../input1.txt");
+        let result = part1(input);
+        assert_eq!(result, "7581");
+    }
+
+}
+
+#[bench]
+fn bench_part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let input = include_str!("../../input1.txt");
+        let _ = part1(input);
+    });
+}
---
+++ day-03/src/bin/day-03-part2.rs
@@ -0,0 +1,71 @@
+#![feature(test)]
+
+extern crate test;
+
+fn item_to_priority(item: char) -> u32 {
+    if item as u32 >= 'a' as u32 {
+        return (item as u32) - ('a' as u32) + 1;
+    }
+
+    (item as u32) - ('A' as u32) + 27
+}
+
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> String {
+    let lines: Vec<&str> = input.lines().collect();
+
+    let priorities: Vec<u32> = (0..lines.len()).step_by(3)
+        .filter_map(|line_idx| {
+            let elf1 = lines[line_idx];
+            let elf2 = lines[line_idx+1];
+            let elf3 = lines[line_idx+2];
+
+            let matching: Vec<char> = elf1.chars()
+                .filter_map(|a| (elf2.find(a).is_some() && elf3.find(a).is_some()).then_some(a))
+                .collect();
+            (matching.len() >= 1).then_some(item_to_priority(matching[0]))
+        })
+        .collect();
+
+    priorities.iter().sum::<u32>().to_string()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part2_example1() {
+        let result = part2(
+            "\
+            vJrwpWtwJgWrhcsFMMfFFhFp\n\
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+            PmmdzqPrVvPwwTWBwg\n\
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+            ttgJtRGJQctTZtZT\n\
+            CrZsJsPPZsGzwwsLwLmpwMDw");
+        assert_eq!(result, "70");
+    }
+
+    #[test]
+    fn test_part2_real_data() {
+        let input = include_str!("../../input1.txt");
+        let result = part2(input);
+        assert_eq!(result, "2525");
+    }
+
+}
+
+#[bench]
+fn bench_part2(b: &mut test::Bencher) {
+    b.iter(|| {
+        let input = include_str!("../../input1.txt");
+        let _ = part2(input);
+    });
+}
